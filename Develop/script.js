8be8b72bda5207b3f3c0e143c3b6928cd0b2689f
William Combee <william.combee@yahoo.com>
2023-03-01 13:49:01 -0600

    changed time display

--- Develop/script.js
+++ Develop/script.js
@@ -15,7 +15,19 @@ $(document).ready(function () {
 
     //set items in local storage.
     localStorage.setItem(time, text);
+
+   
 })
+$("#hour 8 .description").val(localStorage.getItem("#hour 8"));
+$("#hour 9 .description").val(localStorage.getItem("#hour 9"));
+$("#hour 10 .description").val(localStorage.getItem("#hour 10"));
+$("#hour 11 .description").val(localStorage.getItem("#hour 11"));
+$("#hour 12 .description").val(localStorage.getItem("#hour 12"));
+$("#hour 13 .description").val(localStorage.getItem("#hour 13"));
+$("#hour 14 .description").val(localStorage.getItem("#hour 14"));
+$("#hour 15 .description").val(localStorage.getItem("#hour 15"));
+$("#hour 16 .description").val(localStorage.getItem("#hour 16"));
+$("#hour 17 .description").val(localStorage.getItem("#hour 17"));
 
   //
   // TODO: Add code to apply the past, present, or future class to each time
@@ -58,6 +70,13 @@ hourTracker();
   
 
   // TODO: Add code to display the current date in the header of the page.
-  var today = dayjs().format('MMMM D YYYY, h:mm a');
-$('#currentDay').text(today);
+var timeDisplayEl = $('#currentDay');
+
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] h:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+displayTime();
+setInterval(displayTime, 1000);
+
 });
